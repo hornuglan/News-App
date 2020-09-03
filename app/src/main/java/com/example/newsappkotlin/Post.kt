c51ddeaf1ff78537106ae992532560b1e184dc23
hornuglan <rita@nextmail.ru>
2020-09-03 11:00:24 +0300

    Feature: main activity layout, class and layout for the news item

---
+++ app/src/main/java/com/example/newsappkotlin/Post.kt
@@ -0,0 +1,10 @@
+package com.example.newsappkotlin
+
+class Post(
+    val sectionName: String,
+    val date: String,
+    val title: String,
+    val url: String,
+    val authorFirstName: String,
+    val authorLastName: String
+)
